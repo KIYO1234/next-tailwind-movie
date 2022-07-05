ee221bac4d522b7073bef6b04cc14c4cdb3ceb04
KIYO1234 <getstarted3601@gmail.com>
2022-07-05 23:18:10 +0900

    commit1 from rebase3

--- components/Input.tsx
+++ components/Input.tsx
@@ -8,6 +8,7 @@ const Input = () => {
   const [session, loading] = useSession();
   console.log("session.sub: ", session.sub);
   console.log("git rebase from feature/rebase");
+  console.log("rebase (rebase3 branch)");
 
   const titleRef = useRef(null);
   const imageRef = useRef(null);
