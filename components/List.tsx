d3cab53f3df32e4f7d62cba87de139b7f9e0a233
KIYO1234 <getstarted3601@gmail.com>
2022-07-05 23:24:42 +0900

    commit3 from main after rebase

--- components/List.tsx
+++ components/List.tsx
@@ -7,7 +7,7 @@ type Props = {
 };
 
 const List: React.FC<Props> = (props) => {
-  console.log("List (change from main branch): ", List);
+  console.log("List (change from main branch after rebase): ", List);
 
   return (
     <ul>
