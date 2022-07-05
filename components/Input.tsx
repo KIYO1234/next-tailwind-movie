80b5142778dc6667c168eaaf53c5a29b28c2ca6f
KIYO1234 <getstarted3601@gmail.com>
2022-07-05 23:11:51 +0900

    console.log

--- components/Input.tsx
+++ components/Input.tsx
@@ -6,7 +6,7 @@ import { useSession } from "next-auth/client";
 
 const Input = () => {
   const [session, loading] = useSession();
-  // console.log('session.sub: ', session.sub);
+  console.log("session.sub: ", session.sub);
 
   const titleRef = useRef(null);
   const imageRef = useRef(null);
