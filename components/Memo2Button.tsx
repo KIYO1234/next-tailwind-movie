91ff08177bbc2ca60c8d4fbae81ab391e0e79c56
KIYO1234 <getstarted3601@gmail.com>
2022-07-13 22:00:20 +0900

    change1 from pull branch

--- components/Memo2Button.tsx
+++ components/Memo2Button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const Memo2Button = () => {
   console.log("Memo2Button");
+  console.log("change from pull branch");
 
   return <button className="text-purple-500">Memo2Button</button>;
 };
